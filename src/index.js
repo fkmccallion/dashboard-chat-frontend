7a38d0932777a419fe7bc7c88f7c492dee5a1e01
github username <fkmccallion@gmail.com>
2021-01-14 06:35:09 -0800

    create and add rootReducer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
 const store = createStore(
   rootReducer,
@@ -17,7 +18,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
         <App />
-    </Provider>    
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
